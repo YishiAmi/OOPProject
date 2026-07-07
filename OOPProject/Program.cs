0fe527a48ea60315bd8ce4f8d8ffa36d2e127f71
Ammar Mustaqim <153736200+HydraGamingZ@users.noreply.github.com>
2026-07-07 21:55:24 +0800

    Update Program.cs

--- OOPProject/Program.cs
+++ OOPProject/Program.cs
@@ -74,7 +74,99 @@ namespace rpg
         Defense += 2;
         Speed += 1;
     }
+}
 
+public class Enemy
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Speed { get; set; }
+}
+
+public class Boss
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int Attack { get; set; }
+    public string UltimateSkill { get; set; }
+}
+
+public class Weapon
+{
+    public string Name { get; set; }
+    public int Damage { get; set; }
+}
+
+public class Armor
+{
+    public string Name { get; set; }
+    public int Defense { get; set; }
+}
+
+public class Skill
+{
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public string Description { get; set; }
+}
+
+public class UltimateSkill
+{
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public int Cooldown { get; set; }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public class Potion
+{
+    public string Name { get; set; }
+    public int HealAmount { get; set; }
+}
+
+public class Inventory
+{
+    public int Gold { get; set; }
+    public string Items { get; set; }
+}
+
+public class Quest
+{
+    public string Title { get; set; }
+    public string Objective { get; set; }
+    public bool Completed { get; set; }
+}
+
+public class NPC
+{
+    public string Name { get; set; }
+    public string Dialogue { get; set; }
+}
+
+public class Shop
+{
+    public string ShopName { get; set; }
+    public string ItemsAvailable { get; set; }
+}
+
+public class Map
+{
+    public string MapName { get; set; }
+    public int Difficulty { get; set; }
+}
+
+public class GameManager
+{
+    public string GameStatus { get; set; }
+    public int CurrentLevel { get; set; }
+}
     
 
     
