a348753d797af377b6dc545c3d0862904ab7a9cf
Ammar Mustaqim <153736200+HydraGamingZ@users.noreply.github.com>
2026-07-26 21:16:10 +0800

    Update Item.cs

--- RpgLibrary/Items/Item.cs
+++ RpgLibrary/Items/Item.cs
@@ -1,12 +1,44 @@
-using RpgLibrary.Contracts;
-
-namespace RpgLibrary.Items;
-
-public class Item : IShopItem
+namespace RPGGameLibrary.Items
 {
-    public string Name { get; set; } = string.Empty;
+    public class Item
+    {
+        public string Name { get; set; }
 
-    public string Description { get; set; } = string.Empty;
+        public string Description { get; set; }
 
-    public int Price { get; set; }
+        public int Value { get; set; }
+
+
+        public Item(
+            string name,
+            string description,
+            int value)
+        {
+            Name = name;
+            Description = description;
+            Value = value;
+        }
+
+
+        public virtual void Use()
+        {
+            Console.WriteLine($"Using {Name}");
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine(
+                $"Item: {Name}"
+            );
+
+            Console.WriteLine(
+                $"Description: {Description}"
+            );
+
+            Console.WriteLine(
+                $"Value: {Value}"
+            );
+        }
+    }
 }
