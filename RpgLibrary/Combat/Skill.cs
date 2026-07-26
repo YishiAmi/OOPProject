769b563fb18e31de2ec4be2264e94917e37575ae
Mohd Harith Bin Abd Manan <149550207+YishiAmi@users.noreply.github.com>
2026-07-26 20:49:55 +0800

    Refactor Skill class to support attack and heal skills

--- RpgLibrary/Combat/Skill.cs
+++ RpgLibrary/Combat/Skill.cs
@@ -1,10 +1,48 @@
-namespace RpgLibrary.Combat;
+using System;
 
-public class Skill
+namespace rpg
 {
-    public string Name { get; set; } = string.Empty;
 
-    public int Damage { get; set; }
+    public abstract class Skill
+    {
+        public string Name {get;set;}
+        public int Power {get;set;}
 
-    public string Description { get; set; } = string.Empty;
+        protected Skill(string name, int power)
+        {
+            Name = name;
+            Power = power;
+        }
+        public abstract void Use(ICombat source, ICombat target); //source is the the on who is using the skill while target is the one who is receiving it
+    }
+
+
+    public class AttackSkill : Skill
+    {
+    
+        public AttackSkill(string name, int power) : base(name, power)
+        {
+            
+        }
+
+        public override void Use(ICombat source, ICombat target)
+        {
+            Console.WriteLine($"{source.Name} uses {Name} on {target.Name}!");
+            target.TakeDamage(Power);
+        }
+    }
+
+    public class HealSkill : Skill
+    {
+        public HealSkill(string name, int healAmount) : base(name, healAmount)
+        {
+            
+        }
+
+        public override void Use(ICombat source, ICombat target)
+        {
+            Console.WriteLine($"{source.Name} heals with {Name} {Power} HP");
+            source.Heal(Power);   // heal SELF, ignore target
+        }
+    }
 }
