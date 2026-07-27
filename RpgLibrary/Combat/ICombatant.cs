583d847c3e0af3553c7d39dd58fcdf0fcba05861
Mohd Harith Bin Abd Manan <149550207+YishiAmi@users.noreply.github.com>
2026-07-27 21:38:25 +0800

    Remove ICombatant interface definition

--- RpgLibrary/Combat/ICombatant.cs
+++ RpgLibrary/Combat/ICombatant.cs
@@ -1,18 +1,1 @@
-using System;
 
-namespace rpg
-{
-    public interface ICombat
-    {
-        // Read-only info about the fighter
-        string Name { get; }
-        int Health { get; }
-        int MaxHealth { get; }
-        int Defense { get; }
-
-        // things that the fighters needs to have in their code
-        bool IsAlive();
-        void TakeDamage(int damage);
-        void Heal(int amount);
-    }
-}
