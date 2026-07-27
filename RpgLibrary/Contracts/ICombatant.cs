b75a2aa1f9262fd0060302cd95460ae25c13416b
harithyish-debug <harithyish@gmail.com>
2026-07-27 20:56:33 +0800

    Update ICombatant.cs

--- RpgLibrary/Contracts/ICombatant.cs
+++ RpgLibrary/Contracts/ICombatant.cs
@@ -1,12 +1,18 @@
-namespace RpgLibrary.Contracts;
+using System;
 
-public interface ICombatant
+namespace rpg
 {
-    string Name { get; }
+    public interface ICombat
+    {
+        // Read-only info about the fighter
+        string Name { get; }
+        int Health { get; }
+        int MaxHealth { get; }
+        int Defense { get; }
 
-    int Health { get; }
-
-    bool IsAlive();
-
-    void TakeDamage(int amount);
+        // things that the fighters needs to have in their code
+        bool IsAlive();
+        void TakeDamage(int damage);
+        void Heal(int amount);
+    }
 }
