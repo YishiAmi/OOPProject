c5abd3089f785437f316e101d4e7bc433d9a4db2
Ammar Mustaqim <153736200+HydraGamingZ@users.noreply.github.com>
2026-07-28 11:40:24 +0800

    Update Armor.cs

--- RpgLibrary/Items/Armor.cs
+++ RpgLibrary/Items/Armor.cs
@@ -3,30 +3,24 @@ namespace RPGGameLibrary.Items
     public class Armor : Item
     {
         public int Defense { get; set; }
-
         public string ArmorType { get; set; }
-
-
         public Armor(
             string name,
             string description,
-            int value,
+            int price,
             int defense,
             string armorType
         )
-        : base(name, description, value)
+        : base(name, description, price)
         {
             Defense = defense;
             ArmorType = armorType;
         }
-
-
         public override void Use()
         {
             Console.WriteLine(
                 $"Equipped {Name}"
             );
-
             Console.WriteLine(
                 $"Defense +{Defense}"
             );
